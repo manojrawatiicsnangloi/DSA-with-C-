65c5b8e2b1f103997b5e92b8ec91139b29f8205e
abhijeet182002 <manojrawat87634@gmail.com>
2025-09-13 12:55:49 +0530

    inital commit

--- LinkedList/LinkedListInsertion.cpp
+++ LinkedList/LinkedListInsertion.cpp
@@ -1,6 +1,5 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
-using namespace std;
-
 
 class Node{
     public : int data;
@@ -25,13 +24,23 @@ void insertAtEnd(Node *&head, int value){
    
     temp -> next = newNode;
 }
+void deleteAtEnd(Node *&head){
+    if (head == nullptr) return;
+    Node * temp = head;
+    while (temp -> next -> next != nullptr){
+        temp = temp->next;
+    } 
+    delete temp -> next;
+    temp -> next = nullptr;
+}
+
 void printLinkedList(Node * head){
     Node * temp = head;
     while (temp != nullptr){
-        cout << temp -> data << " -> ";
+        std::cout << temp -> data << " -> ";
         temp = temp -> next;
     }
-    cout << " nullptr ";
+    std::cout << " nullptr \n";
 }
 
 void insertAtHead(Node * & head, int value){
@@ -44,6 +53,15 @@ void insertAtHead(Node * & head, int value){
     head = newNode;
     head -> next = temp;
 }
+void deleteAtHead(Node * & head){
+    if (head == nullptr){
+        return;
+    }
+    Node * temp = head;
+    head = head-> next;
+    delete temp;
+}
+
 void insertAtNthPosition(Node * head, int value, int index){
     Node * newNode = new Node(value);
     if (index == 0){
@@ -66,14 +84,46 @@ void insertAtNthPosition(Node * head, int value, int index){
     
 }
 
+void deleteAtNthPosition(Node *&head, int index){
+    if (index == 0){
+        deleteAtHead(head);
+        return;
+    }
+    Node * temp = head;
+    int i = 0;
+    
+    while (temp != nullptr && i < index - 1){
+        temp = temp -> next;
+        i++;
+    }
+    
+    if (temp != nullptr && temp -> next != nullptr){
+        Node * nodeToDelete = temp -> next;
+        temp -> next = nodeToDelete -> next;
+        delete nodeToDelete;
+    }
+}
+
 int main() {
        Node * head = nullptr;
        insertAtHead(head, 1);
-       insertAtEnd(head, 40);
-       insertAtNthPosition(head, 2, 2);
-       insertAtNthPosition(head, 3, 3);
-       insertAtEnd(head, 98);
-       insertAtEnd(head, 89);
+       insertAtEnd(head, 2);
+       insertAtNthPosition(head, 2, 3);
+       insertAtNthPosition(head, 3, 4);
+       insertAtEnd(head, 5);
+       insertAtEnd(head, 6);
+       insertAtEnd(head, 7);
+       insertAtEnd(head, 8);
+       insertAtEnd(head, 9);
+       insertAtEnd(head, 10);
+       insertAtEnd(head, 11);
+       
+       printLinkedList(head);
+       deleteAtHead(head);
+       printLinkedList(head);
+       deleteAtEnd(head);
+       printLinkedList(head);
+       deleteAtNthPosition(head, 3);
        printLinkedList(head);
     return 0;
 }
