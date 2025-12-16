291c0e3ccb677e2b1b9e09c94479b4f1bbd6bfd3
manojrawat87634 <manojrawat87634@gmail.com>
2025-12-16 18:48:21 +0530

    inital commit

--- graphs/graphsTopics/weighted_graphs.cpp
+++ graphs/graphsTopics/weighted_graphs.cpp
@@ -1,42 +1,3 @@
-// #include <iostream>
-// #include <vector>
-// #include <unordered_map>
-// #include <string>
-
-// using namespace std;
-
-// class Graphs{
-//     private: unordered_map <string, vector<pair<string, int>>>G;
-//    public: void addEdge(string &u, string &v,  int weight ){
-//         G[u].push_back({v, weight});
-//         G[v].push_back({u, weight});
-//     }
-    
-//     void printGraph() {
-//         for (const auto& node : G) {
-//             cout << node.first << " -> ";
-//             for (const auto& edge : node.second) {
-//                 cout << "(" << edge.first << ", " << edge.second << ") ";
-//             }
-//             cout << endl;
-//         }
-//     }
-// };
-
-// int main() {
-//     Graphs graph;
-
-//     graph.addEdge("A", "B", 5);
-//     graph.addEdge("A", "C", 3);
-//     graph.addEdge("B", "C", 2);
-//     graph.addEdge("C", "D", 7);
-
-//     graph.printGraph();
-
-//     return 0;
-// }
-
-
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -44,21 +5,13 @@
 
 using namespace std;
 
-class Graphs {
-private:
-    // Adjacency list: node -> (neighbor, weight)
-    unordered_map<string, vector<pair<string, int>>> G;
-
-public:
-    // Add edge (undirected by default)
-    void addEdge(const string& u, const string& v, int weight, bool directed = false) {
+class Graphs{
+    private: unordered_map <string, vector<pair<string, int>>>G;
+   public: void addEdge(string u, string v,  int weight ){
         G[u].push_back({v, weight});
-        if (!directed) {
-            G[v].push_back({u, weight});
-        }
+        G[v].push_back({u, weight});
     }
-
-    // Print the weighted graph
+    
     void printGraph() {
         for (const auto& node : G) {
             cout << node.first << " -> ";
@@ -82,3 +35,50 @@ int main() {
 
     return 0;
 }
+
+
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// #include <string>
+
+// using namespace std;
+
+// class Graphs {
+// private:
+//     // Adjacency list: node -> (neighbor, weight)
+//     unordered_map<string, vector<pair<string, int>>> G;
+
+// public:
+//     // Add edge (undirected by default)
+//     void addEdge(const string& u, const string& v, int weight, bool directed = false) {
+//         G[u].push_back({v, weight});
+//         if (!directed) {
+//             G[v].push_back({u, weight});
+//         }
+//     }
+
+//     // Print the weighted graph
+//     void printGraph() {
+//         for (const auto& node : G) {
+//             cout << node.first << " -> ";
+//             for (const auto& edge : node.second) {
+//                 cout << "(" << edge.first << ", " << edge.second << ") ";
+//             }
+//             cout << endl;
+//         }
+//     }
+// };
+
+// int main() {
+//     Graphs graph;
+
+//     graph.addEdge("A", "B", 5);
+//     graph.addEdge("A", "C", 3);
+//     graph.addEdge("B", "C", 2);
+//     graph.addEdge("C", "D", 7);
+
+//     graph.printGraph();
+
+//     return 0;
+// }
