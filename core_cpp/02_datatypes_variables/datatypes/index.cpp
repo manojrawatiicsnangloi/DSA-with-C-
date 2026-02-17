2456e1bf13498082c5606b6a826ab19573257eef
Manoj Rawat <manojrawat87634@gmail.com>
2026-02-17 17:02:59 +0530

    inital commit

---
+++ core_cpp/02_datatypes_variables/datatypes/index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Integer : " << 1 << std::endl;
+    std::cout << "Float : " << 1.45 << std::endl;
+    std::cout << "Double : " << 1.34234228738632 << std::endl;
+    std::cout << "Long : " << 12391827L << std::endl;
+    std::cout << "Char : " << 'A' << std::endl;
+    std::cout << "Bool : " << true << std::endl;
+    std::cout << "String : " << "Manoj Rawat\n" << std::endl;
+    return 0;
+}
---
+++ core_cpp/02_datatypes_variables/variables/index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int n = 1;
+    float n1 = 1.45;
+    double n2 = 1.34234228738632;
+    long n3 = 12391827L;
+    char ch = 'A';
+    bool isValid = true;
+    std::string name = "Manoj Rawat \n";
+    std::cout << "Name : " << name;
+    std::cout << "Integer : " << n << std::endl;
+    std::cout << "Double : " << n2 << std::endl;
+    std::cout << "Long : " << n3 << std::endl;
+    std::cout << "Ch : " << ch << std::endl;
+    std::cout << "IsValid : " << isValid << std::endl;
+    return 0;
+}
