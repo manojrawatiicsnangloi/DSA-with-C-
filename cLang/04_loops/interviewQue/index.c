a569f65d12b5aa72be05297723b7c97e140c40bc
unknown <manojrawat87634@gmail.com>
2026-03-28 16:55:24 +0530

    inital comit

--- cLang/04_loops/interviewQue/index.c
+++ cLang/04_loops/interviewQue/index.c
@@ -4,10 +4,10 @@ int main(){
     int num;
     printf("Enter number : ");
     scanf("%d",&num);
-    for (int i = 1; i < 10; i++){
-        printf("%d * %d = %d \n", num, i, num * i);
-    }
-
-    
+    for (int i = 1; i <= num; i++){
+        if (i % 2 == 0){
+            printf("%d \n", i);
+        }
+    }   
     return 0;
 }
--- dsa_with_cpp/07_graphs/06_traversal/bsf/index.cpp
+++ dsa_with_cpp/07_graphs/06_traversal/bsf/index.cpp
@@ -1,63 +1,64 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <queue>
+    #include <iostream>
+    #include <unordered_map>
+    #include <vector>
+    #include <queue>
 
-using namespace std;
+    using namespace std;
 
-class graphs_bsf{
-    private:unordered_map<string, vector<pair<string, int>>> root;
-    public:
-    void addEdge(string u, string v, int w){
-        root[u].push_back({v, w});
-        root[v].push_back({u, w});
-    }
-
-    void bfs(string s){
-        queue<string> q;
-        unordered_map <string, bool> vis;
-        q.push(s);
-        vis[s] = true;
-        while (!q.empty()){
-            string current = q.front(); 
-            q.pop();
-            cout << current << " ";
-            for (auto &i : root[current]){
-                if (!vis[i.first]){
-                    q.push(i.first);
-                    vis[i.first] = true;
-                }
-            }          
+    class graphs_bsf{
+        private:unordered_map<string, vector<pair<string, int>>> root;
+        public:
+        void addEdge(string u, string v, int w){
+            root[u].push_back({v, w});
+            root[v].push_back({u, w});
         }
-    }
-    
-    void printGraph(){
-        for (auto &i : root){
-            cout << i.first << " : ";
-            for (pair<string, int> j : i.second){
-                cout << "(" << j.first << ", " << j.second << "), ";
+
+        void bfs(string s){
+            queue<string> q;
+            unordered_map <string, bool> vis;
+            q.push(s);
+            vis[s] = true;
+            while (!q.empty()){
+                string current = q.front(); 
+                q.pop();
+                cout << current << " ";
+                for (auto &i : root[current]){
+                    if (!vis[i.first]){
+                        q.push(i.first);
+                        vis[i.first] = true;
+                    }
+                }          
             }
-            cout << "\n";
         }
+        
+        void printGraph(){
+            for (auto &i : root){
+                cout << i.first << " : ";
+                for (pair<string, int> j : i.second){
+                    cout << "(" << j.first << ", " << j.second << "), ";
+                }
+                cout << "\n";
+            }
+        }
+    };
+
+
+    int main(){
+        graphs_bsf gp;
+
+    gp.addEdge("D","A", 4);
+        gp.addEdge("A","E", 4);
+        gp.addEdge("D","E", 2);
+        gp.addEdge("A","C", 3);
+        gp.addEdge("E","G", 5);
+        gp.addEdge("E","C", 4);
+        gp.addEdge("C","B", 3);
+        gp.addEdge("C","F", 4);
+        gp.addEdge("G","C", 5);
+        gp.addEdge("F","B", 2);
+        gp.addEdge("G","F", 5);
+
+        gp.printGraph();
+        gp.bfs("D");
+        return 0;
     }
-};
-
-
-int main(){
-    graphs_bsf gp;
-
-gp.addEdge("D","A", 4);
-    gp.addEdge("A","E", 4);
-    gp.addEdge("D","E", 2);
-    gp.addEdge("A","C", 3);
-    gp.addEdge("E","C", 4);
-    gp.addEdge("C","B", 3);
-    gp.addEdge("C","F", 4);
-    gp.addEdge("G","C", 5);
-    gp.addEdge("F","B", 2);
-    gp.addEdge("G","F", 5);
-
-    gp.printGraph();
-    gp.bfs("D");
-    return 0;
-}
