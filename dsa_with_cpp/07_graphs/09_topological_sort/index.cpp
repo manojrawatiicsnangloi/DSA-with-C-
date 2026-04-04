d7831b7fb6329a90e265506eed078760b8eac996
unknown <manojrawat87634@gmail.com>
2026-04-04 16:00:26 +0530

    inital commit

--- dsa_with_cpp/07_graphs/09_topological_sort/index.cpp
+++ dsa_with_cpp/07_graphs/09_topological_sort/index.cpp
@@ -10,25 +10,51 @@ class graphs
 
 private:
     unordered_map<string, vector<pair<string, int>>> root;
-    void _dfs(string &node, unordered_map<string, bool>&visited){
-        if(!visited[node]){
-            visited[node] = true;
-            _dfs(node, visited);
+    void _dfs(string &node, unordered_map<string, bool> &visited)
+    {
+        cout << node << " ";
+        for (pair<string, int> &i : root[node])
+        {
+            if (!visited[i.first])
+            {
+                visited[i.first] = true;
+                _dfs(i.first, visited);
+            }
         }
     }
+
 public:
     void insert(string u, string v, int w)
     {
         root[u].push_back({v, w});
     }
 
-    void dfs(string s){
+    void dfs(string s)
+    {
         unordered_map<string, bool> visited;
+        cout << "\nDFS ";
         visited[s] = true;
         _dfs(s, visited);
     }
 
-    void bfs(){}
+    void bfs(string s) {
+        cout << "\nBFS ";
+        queue<string> qu;
+        unordered_map <string, bool> visited;
+        qu.push(s);
+        visited[s] = true;
+        while (!qu.empty()){
+            string node = qu.front();
+            qu.pop();
+            cout << node << " ";
+            for (pair<string, int> &i : root[node]){
+                if (!visited[i.first]){
+                    qu.push(i.first);
+                    visited[i.first] = true;
+                }
+            }
+        }
+    }
 
     void printGraph()
     {
@@ -39,12 +65,29 @@ public:
             {
                 cout << "(" << j.first << ", " << j.second << ") ";
             }
+            cout << "\n";
         }
     }
 };
 
 int main()
 {
+    graphs g;
+    g.insert("D", "A", 4);
+    g.insert("D", "E", 2);
+    g.insert("A", "E", 4);
+    g.insert("A", "C", 3);
+    g.insert("E", "C", 4);
+    g.insert("E", "G", 5);
+    g.insert("C", "B", 3);
+    g.insert("C", "F", 4);
+    g.insert("C", "G", 5);
+    g.insert("G", "C", 5);
+    g.insert("G", "F", 5);
 
+    g.printGraph();
+
+    g.bfs("A");
+    g.dfs("A");
     return 0;
 }
