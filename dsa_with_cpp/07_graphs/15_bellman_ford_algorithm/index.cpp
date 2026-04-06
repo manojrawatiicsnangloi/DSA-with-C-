3dce3ca436f00bf44bedbe8935c47473afb02f6f
unknown <manojrawat87634@gmail.com>
2026-04-06 15:34:00 +0530

    inital commit

--- dsa_with_cpp/07_graphs/15_bellman_ford_algorithm/index.cpp
+++ dsa_with_cpp/07_graphs/15_bellman_ford_algorithm/index.cpp
@@ -6,48 +6,110 @@
 #include <vector>
 using namespace std;
 
-class graphs{
-    private: unordered_map<string, vector<pair<string, int>>>root;
-    void _dfs(string node, unordered_map<string, bool> &vis){
+class graphs
+{
+private:
+    unordered_map<string, vector<pair<string, int>>> root;
+    void _dfs(string node, unordered_map<string, bool> &vis)
+    {
         vis[node] = true;
-        for (auto &i: root[node]){
-            if (!vis[i.first]){
+        for (auto &i : root[node])
+        {
+            if (!vis[i.first])
+            {
                 _dfs(i.first, vis);
             }
         }
     }
 
-    public:
-    void insert(string u, string v, int w){
+
+    
+
+public:
+    void dijkstra(string s){
+        priority_queue<
+            pair<int, string>, 
+            vector<pair<int, string>>,
+            greater<pair<int, string>>
+            > pq;
+            unordered_map<string, int> dist;
+            for (auto&i: root){
+                dist[i.first] = INT_MAX;
+            }
+            pq.push({0, s});
+            dist[s] = 0;
+            while (!pq.empty()){
+                pair<int, string> top = pq.top();
+                pq.pop();
+                  if (top.first > dist[top.second]) continue;  
+                for (auto &i : root[top.second]){
+                    string neigh = i.first;
+                    int current_dis = i.second;
+                    if (current_dis + dist[top.second] < dist[neigh]){
+                        pq.push({current_dis + dist[top.second], i.first});
+                        dist[i.first] = current_dis + dist[top.second];
+                    }
+                }
+            }
+
+            
+            for (auto&i: dist){
+                cout<< i.first << " : " << i.second << "\n";
+            }
+    }
+    void insert(string u, string v, int w)
+
+
+    {
         root[u].push_back({v, w});
-        if (root.find(v) == root.end()){
+        if (root.find(v) == root.end())
+        {
             root[v] = {};
         }
     }
 
-    void dfs(string s){
+    void dfs(string s)
+    {
         unordered_map<string, bool> vis;
         _dfs(s, vis);
     }
 
-    void bfs(string s){
+        void bfs(string s)
+    {
         unordered_map<string, bool> vis;
         queue<string> q;
         q.push(s);
         vis[s] = true;
-        while(!q.empty()){
+        while (!q.empty())
+        {
             string node = q.front();
             q.pop();
             cout << node << " ";
-            for (auto & i: root[node]){
-                if (!vis[i.first]){
+            for (auto &i : root[node])
+            {
+                if (!vis[i.first])
+                {
                     q.push(i.first);
                     vis[i.first] = true;
                 }
-            } 
+            }
         }
     }
 };
-int main(){
+int main()
+{
+    graphs gp;
+    gp.insert("D", "A", 4);
+    gp.insert("A", "E", 4);
+    gp.insert("D", "E", 2);
+    gp.insert("A", "C", 3);
+    gp.insert("E", "C", 4);
+    gp.insert("C", "B", 3);
+    gp.insert("C", "F", 4);
+    gp.insert("G", "C", 5);
+    gp.insert("F", "B", 2);
+    gp.insert("G", "F", 5);
+    // gp.printGraph();
+    gp.dijkstra("D");
     return 0;
 }
