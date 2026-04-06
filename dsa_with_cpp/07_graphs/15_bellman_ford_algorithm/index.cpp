dadcaf788ba43b5fa224f6cc798e71421578eb08
unknown <manojrawat87634@gmail.com>
2026-04-06 14:47:36 +0530

    inital commit

--- dsa_with_cpp/07_graphs/15_bellman_ford_algorithm/index.cpp
+++ dsa_with_cpp/07_graphs/15_bellman_ford_algorithm/index.cpp
@@ -8,6 +8,16 @@ using namespace std;
 
 class graphs{
     private: unordered_map<string, vector<pair<string, int>>>root;
+    void _dfs(string node, unordered_map<string, bool> &vis){
+        vis[node] = true;
+        for (auto &i: root[node]){
+            if (!vis[i.first]){
+                _dfs(i.first, vis);
+            }
+        }
+    }
+
+    public:
     void insert(string u, string v, int w){
         root[u].push_back({v, w});
         if (root.find(v) == root.end()){
@@ -16,11 +26,26 @@ class graphs{
     }
 
     void dfs(string s){
-
+        unordered_map<string, bool> vis;
+        _dfs(s, vis);
     }
+
     void bfs(string s){
         unordered_map<string, bool> vis;
-         
+        queue<string> q;
+        q.push(s);
+        vis[s] = true;
+        while(!q.empty()){
+            string node = q.front();
+            q.pop();
+            cout << node << " ";
+            for (auto & i: root[node]){
+                if (!vis[i.first]){
+                    q.push(i.first);
+                    vis[i.first] = true;
+                }
+            } 
+        }
     }
 };
 int main(){
