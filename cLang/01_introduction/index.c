26e97200e6f5451210666fdf4f7f89e6344dc7b7
unknown <manojrawat87634@gmail.com>
2026-04-07 09:28:34 +0530

    inital comit

---
+++ cLang/01_introduction/index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int a = 21;
+	char ch = 'A';
+	float n1 = 3.14;
+	double n2 = 324.43434343;
+
+	char st [100];
+	printf("Enter your name : ");
+	scanf("%s", &st);
+	printf("%s", st);
+	printf("%d", a);
+	printf("%c", ch);
+	printf("%f", n1);
+	printf("%lf", n2);
+	
+	printf("my first c program");
+	return 0;
+}
