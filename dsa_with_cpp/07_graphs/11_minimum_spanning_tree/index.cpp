ab6e78de66e8d694cd12d7d9175c1e95e39cdcad
unknown <manojrawat87634@gmail.com>
2026-04-09 13:30:22 +0530

    inital commit

--- dsa_with_cpp/07_graphs/11_minimum_spanning_tree/index.cpp
+++ dsa_with_cpp/07_graphs/11_minimum_spanning_tree/index.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
-class graphs{
-private: 
+class graphs
+{
+private:
     unordered_map<string, vector<pair<string, int>>> root;
 
     // DSU structures
@@ -14,26 +16,32 @@ private:
     unordered_map<string, int> rank;
 
     // Find with path compression
-    string find(string x){
-        if(parent[x] != x)
+    string find(string x)
+    {
+        if (parent[x] != x)
             parent[x] = find(parent[x]);
         return parent[x];
     }
 
     // Union by rank
-    void unite(string a, string b){
+    void unite(string a, string b)
+    {
         string pa = find(a);
         string pb = find(b);
 
-        if(pa == pb) return;
+        if (pa == pb)
+            return;
 
-        if(rank[pa] < rank[pb]){
+        if (rank[pa] < rank[pb])
+        {
             parent[pa] = pb;
         }
-        else if(rank[pb] < rank[pa]){
+        else if (rank[pb] < rank[pa])
+        {
             parent[pb] = pa;
         }
-        else{
+        else
+        {
             parent[pb] = pa;
             rank[pa]++;
         }
@@ -41,31 +49,62 @@ private:
 
 public:
     // Undirected graph
-    void add(const string &u, const string &v, const int &w){
+    void add(const string &u, const string &v, const int &w)
+    {
         root[u].push_back({v, w});
         root[v].push_back({u, w});
     }
 
-    void printGraph(){
-        for (auto &i : root){
+    void printGraph()
+    {
+        for (auto &i : root)
+        {
             cout << i.first << " : ";
-            for (auto &j: i.second){
+            for (auto &j : i.second)
+            {
                 cout << "(" << j.first << ", " << j.second << ") ";
             }
             cout << "\n";
         }
     }
 
-    void mst_prism(){
-        unordered_map <string, bool> vis;
-         priority_queue<
-        tuple<int, string, string>, 
-        vector<tuple<int, string, string>>, 
-        greater<tuple<int, string, string>>
-    > pq;
+    void mst_prism(string start)
+    {
+        unordered_map<string, bool> vis;
+        priority_queue<
+            tuple<int, string, string>,
+            vector<tuple<int, string, string>>,
+            greater<tuple<int, string, string>>>
+            pq;
+        pq.push({0, start, "NONE"});
+        
+        int totalCost = 0;
+        while (!pq.empty())
+        {
+            auto top = pq.top();
+            int weight = get<0>(top);
+            string node = get<1>(top);
+            string parent = get<2>(top);
 
-    
+            pq.pop();
+            if (vis[node])
+                continue;
+            vis[node] = true;
+            totalCost += weight;
 
+            if (parent != "NONE")
+            {
+                cout << parent << " - " << node << " : " << weight << "\n";
+            }
+
+            for (auto &nbr : root[node])
+            {
+                if (!vis[nbr.first])
+                {
+                    pq.push({nbr.second, nbr.first, node});
+                }
+            }
+        }
     }
     //  MST using Kruskal
     // void mst(){
@@ -109,23 +148,24 @@ public:
     // }
 };
 
-int main(){
+int main()
+{
     graphs gp;
 
-    gp.add("D","A", 4);
-    gp.add("A","E", 4);
-    gp.add("D","E", 2);
-    gp.add("A","C", 3);
-    gp.add("E","C", 4);
-    gp.add("C","B", 3);
-    gp.add("C","F", 4);
-    gp.add("G","C", 5);
-    gp.add("F","B", 2);
-    gp.add("G","F", 5);
+    gp.add("D", "A", 4);
+    gp.add("A", "E", 4);
+    gp.add("D", "E", 2);
+    gp.add("A", "C", 3);
+    gp.add("E", "C", 4);
+    gp.add("C", "B", 3);
+    gp.add("C", "F", 4);
+    gp.add("G", "C", 5);
+    gp.add("F", "B", 2);
+    gp.add("G", "F", 5);
 
-    gp.printGraph();
+    // gp.printGraph();
 
     // gp.mst(); //  call MST
-    gp.mst_prism();
+    gp.mst_prism("A");
     return 0;
 }
