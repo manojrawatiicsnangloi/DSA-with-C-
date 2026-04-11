d8bbd15d017e954c0c5210d322872257f03a9bb8
unknown <manojrawat87634@gmail.com>
2026-04-11 09:34:15 +0530

    inital commit

---
+++ dsa_with_cpp/07_graphs/16_A_star_algorithm/index.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class graphs{
+    private:
+    unordered_map <string, vector<pair<string, int>>> root;
+    public:
+    void add(string u, string v, int w){
+        root[u].push_back({v, w});
+        root[v].push_back({u, w});
+    }
+
+    void dikastra(string s){
+        priority_queue<
+        pair<int, string>,
+        vector<pair<int, string>>,
+        greater<pair<int, string>>
+        > pq;
+        unordered_map<string, int> dist;
+
+        for (auto&i: root){
+            dist[i.first] = INT_MAX;
+        }
+        dist[s] = 0;
+        pq.push({0, s});
+        while (!pq.empty()){
+            pair<int, string> top = pq.top();
+            pq.pop();
+            if (top.first > dist[top.second]) continue;
+            for (auto &i: root[top.second]){
+                int totalWeight = top.first + i.second; 
+                if (dist[i.first] > totalWeight){
+                    pq.push({totalWeight, i.first});
+                    dist[i.first] = totalWeight;
+                }
+            }
+        }
+        for (auto &i: dist){
+            cout << endl;
+            cout << i.first << " " << i.second ;
+        }
+    }
+
+    void printGraph(){
+        for (auto &i: root){
+            cout << endl;
+            cout << i.first << " : ";
+            for (auto &j :i.second){
+                cout << "("<<j.first << ", " << j.second << ") ";
+            }
+        }
+    }
+};
+
+int main(){
+    graphs gp;
+    gp.add("D", "A", 4);
+    gp.add("A", "E", 4);
+    gp.add("D", "E", 2);
+    gp.add("A", "C", 3);
+    gp.add("E", "C", 4);
+    gp.add("C", "B", 3);
+    gp.add("C", "F", 4);
+    gp.add("G", "C", 5);
+    gp.add("F", "B", 2);
+    gp.add("G", "F", 5);
+    // gp.printGraph();
+    gp.dikastra("A");
+    return 0;
+}
