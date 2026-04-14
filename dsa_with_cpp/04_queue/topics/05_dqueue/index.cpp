fd2d8a67a0d93a487484f3cc657da8c0f45c2278
unknown <manojrawat87634@gmail.com>
2026-04-14 09:25:32 +0530

    inital commit

--- dsa_with_cpp/04_queue/topics/05_dqueue/index.cpp
+++ dsa_with_cpp/04_queue/topics/05_dqueue/index.cpp
@@ -1,26 +1,111 @@
-#include <stdio.h>
+#include <iostream>
 
-class queue{
-   int* root;
-   int cap;
-   int front;
-   int rear; 
-   public : queue(int c)   {
-    this-> root = new int[c];
-    front = rear = -1;
-    cap = c;
-   } 
-      void enqueue(int value){};   // insert element
-    void dequeue(){};            // remove element
-    int getFront(){};            // see front element
+class deque {
+    int* arr;
+    int cap;
+    int front;
+    int rear;
+
+public:
+    // constructor
+    deque(int c) {
+        arr = new int[c];
+        cap = c;
+        front = rear = -1;
+    }
+
+    // insertion
+    void pushFront(int value) {
+        if (isFull()) return;
+        if (front == -1){
+            front = rear = 0;
+        }
+        else{
+            front = (front - 1 + cap) % cap;
+        }
+        arr[front] = value;
+    }
+
+    void pushRear(int value) {
+        if (isFull()) return;
+        if (rear == -1){
+            front = rear = 0;
+        }
+        else{
+            rear = (rear + 1) % cap;
+        }
+        arr[rear] = value;
+    }
+
+    // deletion
+    void popFront() {
+        if (isEmpty()) return;
+        if (front == rear){ 
+            front = rear = -1;
+        }
+        else{
+            front = (front + 1) % cap;
+        }
+    }
+
+    void popRear() {
+        if (isEmpty()) return;
+        if (front == rear){
+            front = rear = -1;
+        }
+        else{
+            rear = ((rear - 1) + cap ) % cap;
+        }
+    }
+
+    // access
+    int getFront() {
+        if (front != -1) return arr[front];
+        return -1;
+    }
+
+    int getRear() {
+        if (rear != -1){ return arr[rear];}
+        return -1;
+    }
 
     // utility
-    bool isEmpty(){};
-    bool isFull(){};
-    void display(){};
+   bool isEmpty() {
+    return front == -1;
+}
+
+    bool isFull() {
+         return (front == 0 && rear == cap - 1) || (front == rear + 1);
+    }
+
+    void display() {
+            if (isEmpty()) {
+        std::cout << "Deque is empty\n";
+        return;
+    }
+    int i = front;
+    while (true){
+        std::cout << arr[i] << " ";
+        if (i == rear) break;
+        i = (i + 1) % cap;
+    }
+    std::cout << "\n";
+    }
 };
 
+int main() {
+    deque dq(5);
 
-int main(){
+    dq.pushRear(10);
+    dq.pushRear(20);
+    dq.pushFront(5);
+
+    dq.display();
+
+    dq.popFront();
+    dq.popRear();
+
+    dq.display();
+
     return 0;
 }
