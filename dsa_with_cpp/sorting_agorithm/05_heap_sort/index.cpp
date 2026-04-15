67ae42a5570e56f111ad04149574f1ed9f9ea1fc
unknown <manojrawat87634@gmail.com>
2026-04-15 10:34:45 +0530

    inital commit

---
+++ dsa_with_cpp/sorting_agorithm/05_heap_sort/index.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+    void heapify(int arr[], int i, int size){
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if (left < size && arr[left] > arr[largest]){
+            largest = left;
+        }
+        
+        if (right < size && arr[right] > arr[largest]){
+            largest = right;
+        }
+
+        if (largest != i){
+            std::swap(arr[largest], arr[i]);
+            heapify(arr, largest, size);
+        }
+
+    }
+
+void buildMaxHeap(int arr[], int size){
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(arr, i, size);
+        }    
+        
+        for (int i = size - 1; i >= 0; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, 0, i);
+        }
+    }
+int main(){
+
+    return 0;
+}
