22812fd9836ca61a17941ac043da3a02bbd32f5d
unknown <manojrawat87634@gmail.com>
2026-04-23 17:04:36 +0530

    inital commit

--- dsa_with_cpp/06_tree/types_of_tree/03_btree/index.cpp
+++ dsa_with_cpp/06_tree/types_of_tree/03_btree/index.cpp
@@ -27,8 +27,32 @@ class node{
             C[i]->traversal();
         }
     }
+    void insertAtNonFull(int data){
+        int i = n - 1;
+        if (leaf){
+            while (i >= 0 && data < keys[i]){
+                keys[i + 1] = i;
+                i--;
+            }
+            keys[i + 1] = data;
+            n++;
+        }
+        else{
+            while (i >= 0 && keys[i] > data){
+                i--;
+            }
+            if (C[i + 1]->n == 2*t - 1) {
+                splitChild(i + 1, C[i + 1]);
 
-    
+                if (keys[i + 1] < data){
+                    i++;
+                }
+            }
+            C[i + 1]->insertAtNonFull(data);
+        }        
+    }
+
+    void splitChild(int i, node * n){}
 };
 int main(){
     return 0;
