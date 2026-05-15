9eb9fa0689539d1e4e7bf2b2a5c4910456e0e37f
manojrawat87634 <manojrawat87634@gmail.com>
2026-05-15 11:10:30 +0530

    lru cache

--- revision/02_linked_list/01_LRU_Cache/index.cpp
+++ revision/02_linked_list/01_LRU_Cache/index.cpp
@@ -1,88 +1,79 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
 using namespace std;
 
 class node{
-    public:
-    int key;
-    int value;
-    node * next, *prev;
+    public:int key, value;
+    node * prev;
+    node * next;
     node(int k, int v){
-        this ->key = k;
+        this -> key = k;
         this -> value = v;
-        this -> next = this -> prev = nullptr;
     }
 };
-class LRUCache{
-    int cap;
-    unordered_map <int, node *>cache; 
+
+class lru_cache{
+    int capacity;
+    unordered_map<int, node *> cache;
     node * head;
     node * tail;
-public:
-    LRUCache(int c){
-        this -> head = new node(-1, -1);
-        this -> tail = new node(-1, -1);
-        head -> next = tail;
+    public : lru_cache(int cap){
+        this -> capacity = cap;
+        head = new node(-1, -1);
+        tail = new node(-1, -1);
         tail -> prev = head;
-        this -> cap = c;
+        head -> next = tail;
+    }
+    void addNode(node * n){
+            n-> next = head -> next;
+            n-> prev = head;
+
+            head -> next-> prev = n;
+            head -> next = n;
     }
 
-    void add(node * node){
-        node -> next = head -> next;
-        head -> next = node;
-
-        node -> prev = head;
-        node -> next -> prev = node;
-    }
-
-    
     void removeNode(node * n){
         n -> prev -> next = n -> next;
         n -> next -> prev = n -> prev;
     }
 
-
-    int getCache(int key){
+    int get(int key){
         if (cache.find(key) == cache.end()){
             return -1;
         }
         node * n = cache[key];
         removeNode(n);
-        add(n);
-        return n -> value;
+        addNode(n);
     }
-
     void put(int key, int value){
         if (cache.find(key) != cache.end()){
+            cache.erase(key);
             node * n = cache[key];
+            removeNode(n);
+            delete n;
+        }
+        if (cache.size() == capacity){
+            node * n = tail->prev;
             removeNode(n);
             cache.erase(key);
             delete n;
         }
-        if (cache.size() == cap){
-            node * lru = tail -> prev;
-            removeNode(lru);
-            cache.erase(lru-> key);
-            delete lru;
-        }
-        node * newNode =  new node(key, value);
-        add(newNode);
+        node * newNode = new node(key, value);
+        addNode(newNode);
         cache[key] = newNode;
     }
+
     void printCache(){
-         node* temp = head->next;
-        while (temp != tail) {
-            cout << "(" << temp->key << "," << temp->value << ") ";
-            temp = temp->next;
+        node * temp = head -> next;
+        while (temp != tail){
+            cout << "( " << temp->key << " : " << temp->value << ") \n";
+            temp = temp-> next;
         }
-        cout << endl;
     }
 };
 
 int main(){
-
-    LRUCache cache(3);
+        lru_cache cache(3);
 
     cache.put(1, 10);
     cache.put(2, 20);
@@ -90,12 +81,12 @@ int main(){
 
     cache.printCache(); // (3,30) (2,20) (1,10)
 
-    cache.getCache(1);       // access 1 → becomes MRU
+    cache.get(1);       // access 1 → becomes MRU
     cache.printCache(); // (1,10) (3,30) (2,20)
 
     cache.put(4, 40);   // removes LRU (2)
     cache.printCache(); // (4,40) (1,10) (3,30)
 
-    cout << cache.getCache(2) << endl; 
+    cout << cache.get(2) << endl; 
     return 0;
 }
